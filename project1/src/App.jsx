0342f2fd622e38b465b3d3d4fdfbfcfbf2685c26
Sandesh <sandesh0799@gmail.com>
2024-05-20 15:00:15 +0530

    Design Nike shoe website in react

---
+++ project1/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Hero from './components/Hero';
+
+import Nevigation from './components/Nevigation';
+function App() {
+
+  return (
+    <>
+      <div>
+        <Nevigation />
+        <Hero />
+      </div>
+    </>
+  )
+}
+
+export default App
